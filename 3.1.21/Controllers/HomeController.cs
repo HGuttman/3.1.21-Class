3ccf9d5da3f99d631e846d49308868a14dd4d5e7
Hudi Guttman <hudiguttman@gmail.com>
2021-03-03 20:46:33 -0500

    Add project files.

---
+++ 3.1.21/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using _3._1._21.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3._1._21.Controllers
+{
+    public class HomeController : Controller
+    {
+            public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Submit(string text)
+        {
+            var count = GetCount(text);
+            var vm = new IndexViewModel { Count = count, Text = text };
+            return View(vm);
+        }
+
+        private Dictionary<char, int> GetCount(string text)
+        {
+            Dictionary<char, int> count = new();
+            foreach(char c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+            {
+                count.Add(c, 0);
+            }
+            foreach (var c in text.ToUpper())
+            {
+                if (count.ContainsKey(c))
+                {
+                    count[c]++;
+                }
+                else
+                {
+                    count[c] = 1;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ 3.1.21/Models/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3._1._21.Models
+{
+    public class IndexViewModel
+    {
+        public Dictionary<char, int> Count;
+        public string Text;
+    }
+}
